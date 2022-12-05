9bc3628926cc3c58149eed4c268f17640f5dc689
wwwhoami <subman450@gmail.com>
2022-12-05 18:11:36 +0300

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,14 @@ async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('nest-blog')
+    .setDescription('NextBlog API description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 
