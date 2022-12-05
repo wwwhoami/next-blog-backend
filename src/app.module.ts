b0a1c0ffe43c036e1fa494ffa4913f5dc4d3da59
wwwhoami <subman450@gmail.com>
2022-12-05 18:04:59 +0300

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PostModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
