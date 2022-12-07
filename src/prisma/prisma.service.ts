10833681b874d47133d4bc7f0f1eaf1930151065
wwwhoami <subman450@gmail.com>
2022-12-07 23:28:11 +0300

    prisma logger implemented

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,50 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+  }
+
+  private readonly logger = new Logger(PrismaService.name);
+
   async onModuleInit() {
+    this.$on('query', (e) => {
+      this.logger.verbose(`${e.query} ${e.params} +${e.duration}ms`);
+    });
+
+    this.$on('error', (event) => {
+      this.logger.verbose(event.target);
+    });
+
     await this.$connect();
   }
 
