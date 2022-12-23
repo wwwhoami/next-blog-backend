32ecd08f34c4659c5386e7de759dc1ec95a03744
wwwhoami <subman450@gmail.com>
2022-12-23 19:43:35 +0300

    optional User.image migration

---
+++ prisma/migrations/20221222175314_user_image_optional/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_author_id_fkey";
+
+-- DropIndex
+DROP INDEX "post_title_gist_index";
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "image" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
