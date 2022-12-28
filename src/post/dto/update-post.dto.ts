baa900192652f5964e7baf8736d409237dfabc3a
wwwhoami <subman450@gmail.com>
2022-12-29 02:09:26 +0300

    update-post dto

--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,48 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePostData } from './create-post.dto';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsDefined,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+  ValidateNested,
+} from 'class-validator';
+import { CreateCategoryDto } from 'src/category/dto/create-category.dto';
 
-export class UpdatePostDto extends PartialType(CreatePostData) {}
+export class UpdatePostData {
+  @IsNumber()
+  id: number;
+
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  excerpt?: string;
+
+  @IsOptional()
+  @IsString()
+  content?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  published?: boolean;
+
+  @IsOptional()
+  @IsUrl()
+  coverImage?: string;
+}
+
+export class UpdatePostDto {
+  @IsDefined()
+  @ValidateNested({ each: true })
+  @Type(() => UpdatePostData)
+  post: UpdatePostData;
+
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => CreateCategoryDto)
+  categories?: CreateCategoryDto[];
+}
