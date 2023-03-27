230521e241ea6987e425fac3e5307339e2bb5ca9
wwwhoami <subman450@gmail.com>
2023-03-27 21:24:43 +0300

    test setup & teardown scripts updated

--- test/setup.ts
+++ test/setup.ts
@@ -6,6 +6,11 @@ require('ts-node').register({
 import { PrismaClient } from '@prisma/client';
 import { seedWithMocks } from 'prisma/seed-with-mocks';
 
+const resetAutoIncrement = async (prisma: PrismaClient) => {
+  const resetCommentIdSeq = prisma.$queryRaw`ALTER SEQUENCE "public"."Comment_id_seq" RESTART WITH 1`;
+  await resetCommentIdSeq;
+};
+
 const setup = async () => {
   const prisma = new PrismaClient();
   try {
@@ -14,13 +19,18 @@ const setup = async () => {
     const deletePosts = prisma.post.deleteMany();
     const deletePostToCategory = prisma.postToCategory.deleteMany();
     const deleteCategory = prisma.category.deleteMany();
+    const deleteComments = prisma.comment.deleteMany();
 
     await prisma.$transaction([
       deletePostToCategory,
       deleteUsers,
       deletePosts,
       deleteCategory,
+      deleteComments,
     ]);
+
+    // Reset auto-increment id sequences
+    resetAutoIncrement(prisma);
 
     // Seed database with mocks only
     await seedWithMocks(prisma);
--- test/teardown.ts
+++ test/teardown.ts
@@ -8,16 +8,18 @@ import { PrismaClient } from '@prisma/client';
 const teardown = async () => {
   const prisma = new PrismaClient();
   try {
+    // Cleanup database at the end of tests
     const deleteUsers = prisma.user.deleteMany();
     const deletePosts = prisma.post.deleteMany();
     const deletePostToCategory = prisma.postToCategory.deleteMany();
     const deleteCategory = prisma.category.deleteMany();
-
+    const deleteComments = prisma.comment.deleteMany();
     await prisma.$transaction([
       deletePostToCategory,
       deleteUsers,
       deletePosts,
       deleteCategory,
+      deleteComments,
     ]);
   } catch (err) {
     console.log(err);
