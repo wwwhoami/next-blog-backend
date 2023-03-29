76fc626e7e49bc0dd5f1c7e7b3b48dcaf2220bad
wwwhoami <subman450@gmail.com>
2023-03-29 16:19:41 +0300

    category.repository add jsdoc

--- src/category/category.repository.ts
+++ src/category/category.repository.ts
@@ -16,10 +16,13 @@ export class CategoryRepository {
     private postRepository: PostRepository,
   ) {}
 
-  getMany({
-    take = 25,
-    skip = 0,
-  }: GetCategoryDto): Promise<CategoryNoDescription[]> {
+  /**
+   * @param {GetCategoryDto} getCategoryOptions - Options for getting categories
+   * @description Get categories with no description
+   */
+  getMany({ take = 25, skip = 0 }: GetCategoryDto = {}): Promise<
+    CategoryNoDescription[]
+  > {
     return this.prisma.category.findMany({
       select: {
         name: true,
@@ -30,6 +33,10 @@ export class CategoryRepository {
     });
   }
 
+  /**
+   * @param {FindCategoryDto} findCategoryOptions - Options for finding categories
+   * @description Find categories with description and hotness score
+   */
   async findMany({
     take = 25,
     skip = 0,
@@ -61,6 +68,7 @@ export class CategoryRepository {
       },
     });
 
+    // Format categories to include hotness score
     return categories.map((category) => ({
       name: category.name,
       description: category.description,
@@ -69,6 +77,9 @@ export class CategoryRepository {
     }));
   }
 
+  /**
+   * @description Get all category combinations
+   */
   async getCombinations(): Promise<string[][]> {
     const categoryComb = await this.prisma.$queryRaw<
       Record<'category_list', string>[]
@@ -86,6 +97,7 @@ export class CategoryRepository {
           DISTINCT T.category_list
       FROM T`;
 
+    // Split category list into array of categories for each category
     const categoryLists = categoryComb.map((category) =>
       category.category_list.split(','),
     );
@@ -93,13 +105,20 @@ export class CategoryRepository {
     return categoryLists;
   }
 
+  /**
+   * @param {string} searchTerm - Search term to filter categories by
+   * @description Get category combinations for posts that match search term
+   */
   async getCombinationsForSearchTerm(searchTerm: string): Promise<string[][]> {
+    // Get post ids that match search term
     const postIds = (await this.postRepository.findIds({ searchTerm })).map(
       (post) => post.id,
     );
 
+    // Return empty array if no posts match search term
     if (postIds.length === 0) return [];
 
+    // Get category combinations for posts that match search term
     const categoryComb = await this.prisma.$queryRaw<
       Record<'category_list', string>[]
     >`
@@ -117,6 +136,7 @@ export class CategoryRepository {
             DISTINCT T.category_list
         FROM T`;
 
+    // Split category list into array of categories for each category
     const categoryLists = categoryComb.map((category) =>
       category.category_list.split(','),
     );
@@ -124,6 +144,10 @@ export class CategoryRepository {
     return categoryLists;
   }
 
+  /**
+   * @param {CreateCategoriesDto} category - Categories to create
+   * @description Create categories
+   */
   create(category: CreateCategoriesDto) {
     return this.prisma.category.createMany({
       data: category.categories,
