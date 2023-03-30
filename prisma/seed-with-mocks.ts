590fa990175efb83059968c723bef794664589ba
wwwhoami <subman450@gmail.com>
2023-03-30 19:38:14 +0300

    getPostMocks without ids for seed

--- prisma/seed-with-mocks.ts
+++ prisma/seed-with-mocks.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import { getPostMocksWithId } from 'data/post';
+import { getPostMocks } from 'data/post';
 import { categoryData, commentData, userData } from 'data/seed-data';
 import { PrismaSeeder } from './seeders';
 
 export async function seedWithMocks(prisma: PrismaClient) {
   const prismaSeeder = new PrismaSeeder(prisma);
-  const postData = await getPostMocksWithId();
+
+  const postData = await getPostMocks();
 
   await prismaSeeder.seedUsers(userData);
   await prismaSeeder.seedCategories(categoryData);
