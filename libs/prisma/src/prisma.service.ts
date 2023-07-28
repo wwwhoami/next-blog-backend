84147a3645170614cb045a1f4fa1bd32d61bcb69
wwwhoami <subman450@gmail.com>
2023-07-28 20:08:24 +0300

    prisma_service lib implemented

---
+++ libs/prisma/src/prisma.service.ts
@@ -0,0 +1,57 @@
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, 'query' | 'error'>
+  implements OnModuleInit
+{
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+        {
+          emit: 'stdout',
+          level: 'info',
+        },
+        {
+          emit: 'stdout',
+          level: 'warn',
+        },
+      ],
+    });
+  }
+
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    this.$on('query', (e) => {
+      this.logger.verbose(`${e.query} ${e.params} +${e.duration}ms`);
+    });
+
+    this.$on('error', (event) => {
+      this.logger.verbose(event.target);
+    });
+
+    await this.$queryRaw`SET pg_trgm.similarity_threshold = 0.2`;
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
