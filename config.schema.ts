0006285607429d03b6b292eefe870399dbb3699f
wwwhoami <subman450@gmail.com>
2023-11-20 21:08:06 +0300

    add KAFKA_HOST env var

--- config.schema.ts
+++ config.schema.ts
@@ -17,6 +17,7 @@ export const configValidationSchema = Joi.object({
   APP_CORE_PORT: Joi.number().default(3000),
   APP_WS_NOTIFICATION_PORT: Joi.number().default(3001),
   KAFKA_BROKER_ID: Joi.number().default(1),
+  KAFKA_HOST: Joi.string().required(),
   KAFKA_PORT: Joi.number().default(9092),
   ZOOKEEPER_PORT: Joi.number().default(2181),
   CLIENT_URL: Joi.string(),
