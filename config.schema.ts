02e7f7067f933c11ce09dac33cd3810c22677fc9
wwwhoami <subman450@gmail.com>
2023-11-22 23:02:55 +0300

    Update main.yml

--- config.schema.ts
+++ config.schema.ts
@@ -13,7 +13,7 @@ export const configValidationSchema = Joi.object({
   POSTGRES_DB: Joi.string().required(),
   REDIS_HOST: Joi.string().required(),
   REDIS_PORT: Joi.number().default(6379),
-  REDIS_PASSWORD: Joi.string().required(),
+  REDIS_PASSWORD: Joi.string(),
   APP_CORE_PORT: Joi.number().default(3000),
   APP_WS_NOTIFICATION_PORT: Joi.number().default(3001),
   KAFKA_BROKER_ID: Joi.number().default(1),
