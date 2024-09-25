86c4c43d321ee2c8223ff7c4dbb7e2d21bf5d670
wwwhoami <subman450@gmail.com>
2024-09-25 17:30:28 +0300

    mv docker-compose scripts to package.json

--- scripts/docker-compose-ci-test.sh
+++
@@ -1,1 +0,0 @@
-docker compose -f docker-compose-ci.yml --env-file .env.docker-compose up -d && docker exec -it next-blog-core-app pnpm test:e2e
--- scripts/docker-compose-local.sh
+++
@@ -1,1 +0,0 @@
-docker compose -f docker-compose-local.yml --env-file .env up -d
