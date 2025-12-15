fa0872d876c288b64ae5c8bee7d97acaf3db71ca
wwwhoami <subman450@gmail.com>
2025-12-15 23:28:41 +0300

    fix(prisma): fix prisma service to only log for non-testing environment

--- libs/prisma/src/prisma.service.ts
+++ libs/prisma/src/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -9,7 +10,7 @@ export class PrismaService
   >
   implements OnModuleInit
 {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
       log: [
         {
@@ -35,22 +36,25 @@ export class PrismaService
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
-    this.$on('query', (e) => {
-      e.query = e.query.replace(/(\n|\t|\s)+/g, ' ');
-      this.logger.log(e, `PrismaClient:Query +${e.duration}ms`);
-    });
+    // Only log Prisma queries in non-testing environments
+    if (this.configService.get('NODE_ENV') !== 'testing') {
+      this.$on('query', (e) => {
+        e.query = e.query.replace(/(\n|\t|\s)+/g, ' ');
+        this.logger.log(e, `PrismaClient:Query +${e.duration}ms`);
+      });
 
-    this.$on('error', (e) => {
-      this.logger.error(e.message, e.target, 'PrismaClient:Error');
-    });
+      this.$on('error', (e) => {
+        this.logger.error(e.message, e.target, 'PrismaClient:Error');
+      });
 
-    this.$on('info', (e) => {
-      this.logger.verbose(e, 'PrismaClient:Info');
-    });
+      this.$on('info', (e) => {
+        this.logger.verbose(e, 'PrismaClient:Info');
+      });
 
-    this.$on('warn', (e) => {
-      this.logger.warn(e, 'PrismaClient:Warn');
-    });
+      this.$on('warn', (e) => {
+        this.logger.warn(e, 'PrismaClient:Warn');
+      });
+    }
 
     await this.$queryRaw`SET pg_trgm.similarity_threshold = 0.3`;
     await this.$connect();
