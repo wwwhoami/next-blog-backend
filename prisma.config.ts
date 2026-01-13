66afb7c3dab8145e572bc1632b8d8074a952ad7f
wwwhoami <subman450@gmail.com>
2026-01-13 21:51:11 +0300

    build(docker): optimize Dockerfiles and reduce image size
    - update .dockerignore to exclude prisma client, .env files and some
      config files
    - clean apt cache after package installation
    - add placeholder POSTGRES_URL for Prisma client generation
    - use installed Prisma instead of dlx

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,5 @@
-import '@dotenvx/dotenvx/config'
 import path from 'node:path';
 import { defineConfig, env } from 'prisma/config';
-
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
   migrations: {
